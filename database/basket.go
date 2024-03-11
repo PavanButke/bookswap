00c2c01fab809255d6f0bec0a8fb0892d56fb320
PavanButke <butkepavan55@gmail.com>
2024-03-11 21:38:51 +0530

    let's resatrt

---
+++ database/basket.go
@@ -0,0 +1,68 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"code/models"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var (
+	ErrCantFindBook         = errors.New("can't find the product")
+	ErrCantDecodeBooks      = errors.New("can't find the product")
+	ErrUserIdIsNotValid     = errors.New("this user is invalid")
+	ErrCantUpdateUser       = errors.New("Can not add the books")
+	ErrCantRemoveItemBasket = errors.New("Can not remove this book from basket ")
+	ErrCantGetItem          = errors.New("Was unable to get book from basket")
+	ErrCantBorrowBasketItem = errors.New("can't update the basket")
+)
+
+func AddBookToBasket(ctx context.Context, bookCollection, userCollection *mongo.Collection, bookID primitive.ObjectID, userID string) error {
+	searchfromdb, err := bookCollection.Find(ctx, bson.M{"_id": bookID})
+	if err != nil {
+		log.Println(err)
+		return ErrCantFindBook
+	}
+	var bookbasket []models.BookUser
+	err = searchfromdb.All(ctx, &bookbasket)
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeBooks
+	}
+
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdIsNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "bookbasket", Value: bson.D{{Key: "$each", Value: bookbasket}}}}}}
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return ErrCantUpdateUser
+	}
+	return nil
+}
+
+func RemoveItem() gin.HandlerFunc {
+
+}
+
+// func getItemFromBasket() gin.HandlerFunc {
+
+// }
+
+func BorrowFromBasket() gin.HandlerFunc {
+
+}
+
+func GrabIt() gin.HandlerFunc {
+
+}
---
+++ models/models.go
@@ -0,0 +1,65 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID              primitive.ObjectID `json: "_id": bson:"_id"`
+	First_Name      *string            `json: "first_name"	validate:"required , min=2 , max=25"`
+	Last_Name       *string            `json: "last_name"	validate:"required	, min=2 , max=25"`
+	Password        *string            `json: "password" 	validate:"required , min=6"`
+	Email           *string            `json: "email"		validate:"email , required"`
+	Phone           *string            `json: "phone"`
+	Token           *string            `json: "token"`
+	Refresh_Token   *string            `json: "refresh_token`
+	Created_At      time.Time          `json: "created_at"`
+	Updated_At      time.Time          `json: "updated_at"`
+	User_ID         string             `json: "user_id"`
+	UserBasket      []BookUser         `json: userbasket bson:"userbasket"`
+	Address_Details []Address          `json:"address" bson:"address"`
+	Borrow_Status   []Borrow           `json:"borrow" bson:"borrow"`
+}
+
+type Book struct {
+	Book_ID   primitive.ObjectID `bson:"_id"`
+	Book_Name *string            `json: "book_name" validate:"required"`
+	Author    *string            `json: "author"`
+	Price     *uint64            `json: "price"`
+	//may be a set of arr for img
+	Image           *string `json: "image"	validate:"required"`
+	Recommendations int     `json: "recommendections"`
+}
+
+type BookUser struct {
+	Book_ID   primitive.ObjectID `bson: "_id"`
+	Book_Name *string            `json: "book_name" bson: "book_name"`
+	Author    *string            `json: "author" bson:"author" `
+	Price     *uint64            `json: "price" bson:"price"`
+	Image     *string            `json: "image" bson:"image"`
+}
+
+type Address struct {
+	Address_ID primitive.ObjectID `bson:"_id"`
+	House      *string            `json: "house_name" bson:"house_name"`
+	Street     *string            `json: "street_name" bson:"street_name"`
+	City       *string            `json: "city" bson:"city" validate:"required"`
+	Pincode    *string            `json: "pincode" bson:"pincode"`
+}
+
+type Borrow struct {
+	Borrow_ID     primitive.ObjectID `bson: "_id"`
+	Borrow_Basket []BookUser         `json: "borrow_basket" bson:"borrow_basket"`
+	Return_Date   time.Time          `json: "return_date" bson:"return_date"`
+	Borrowed_At   time.Time          `json: "borrowed_at bson:"borrowed_at"`
+	Price         *uint64            `json: "price" bson:"price"`
+	Discount      int                `json: "discount" bson:"discount"`
+	//Payment_Method Payment            `json: "payment_method" bson:"payment_method"`
+}
+
+// type Payment struct {
+// 	Digital
+// 	COD
+// }
